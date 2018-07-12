eb738a7f8aa1d008052634efb45a21a3fcf242e6
Anastasia <anastasyar1996@gmail.com>
2018-07-12 19:03:30 +0300

    add components, reducer, actions

--- src/containers/SignUp/index.jsx
+++ src/containers/SignUp/index.jsx
@@ -1,76 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Button, Input, Form, Segment } from 'semantic-ui-react';
+import { Button, Form, Segment } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
 
 import Footer from '../../components/Footer/index';
+import FormComponent from './FormComponent';
+import CheckComponent from './CheckComponent';
+
+import { SIGN_IN_PATH } from '../../constants/RouterConstants';
+import { FORM_SIGN_UP } from '../../constants/FormConstants';
 
 class SignUp extends React.Component {
 
 	render() {
+		const { accepted } = this.props;
 
 		return (
 			<Segment basic className="wrapper">
-				<div className="content center-mode ">
+				<div className="content center-mode">
 					<div>
 						<Form className="user-form">
 							<div className="form-info">
 								<h3>Welcome to Echo</h3>
 							</div>
-							<div className="field-wrap">
-								<Form.Field>
-									<label htmlFor="AccountName">Account name (public)</label>
-									<div className="error">
-										<input id="AccountName" className="ui input" placeholder="Account name" />
-										<span className="error-message">
-                                    This name is already exist
-										</span>
-									</div>
-								</Form.Field>
-								<Form.Field>
-									<label htmlFor="GeneretedPas">Generated password</label>
-									<div className="ui action input">
-										<input id="GeneretedPas" className="ui input" placeholder="Genereted password" />
-										<button className="ui orange icon right button">
-											<i aria-hidden="true" className="copy icon" />
-										</button>
-									</div>
-								</Form.Field>
-								<Form.Field>
-									<label htmlFor="ConfirmPas">Confirm password</label>
-									<Input id="ConfirmPas" placeholder="Confirm password" />
-								</Form.Field>
+							<FormComponent />
+							<CheckComponent />
 
-							</div>
-							<div className="check-list">
-								<div className="check orange">
-									<input type="checkbox" id="lose-acces" />
-									<label className="label" htmlFor="lose-acces">
-										<span className="label-text">I understand that I will lose access to my funds if I loose my password</span>
-									</label>
-								</div>
-								<div className="check orange">
-									<input type="checkbox" id="no-recover" />
-									<label className="label" htmlFor="no-recover">
-										<span className="label-text">I understand no one can recover my password if I lose or forget it</span>
-									</label>
-								</div>
-								<div className="check orange">
-									<input type="checkbox" id="strored-pas-true" />
-									<label className="label" htmlFor="strored-pas-true">
-										<span className="label-text">I have written down or otherwise stored my password</span>
-									</label>
-								</div>
-							</div>
-
-
-							<Button basic type="submit" className="disabled" color="orange">Create account</Button>
+							<Button basic type="submit" className={accepted ? '' : 'disabled'} color="orange">Create account</Button>
 							{/* FOR BUTTON WHIT LOADING:
-						<Button type="submit" color="orange"  className="load">Loading...</Button>
-					*/}
+								<Button type="submit" color="orange"  className="load">Loading...</Button>
+							*/}
 							<span className="sign-nav">
-                        Have an account?
-								<Link className="link orange" to="/sign-in"> Login</Link>
+								Have an account?
+								<Link className="link orange" to={SIGN_IN_PATH}> Login</Link>
 							</span>
 						</Form>
 					</div>
@@ -82,4 +46,16 @@ class SignUp extends React.Component {
 
 }
 
-export default connect()(SignUp);
+SignUp.propTypes = {
+	accepted: PropTypes.bool,
+};
+
+SignUp.defaultProps = {
+	accepted: false,
+};
+
+
+export default connect((state) => ({
+	accepted: state.form.getIn([FORM_SIGN_UP, 'accepted']),
+
+}))(SignUp);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import modalReducer from './ModalReducer';
 import globalReducer from './GlobalReducer';
+import formReducer from './FormReducer';
 
 export default {
 	modal: modalReducer.reducer,
 	global: globalReducer.reducer,
+	form: formReducer.reducer,
 };
