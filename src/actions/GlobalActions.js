36a069679a0193e5519bcefc4fd1aa7579539f26
Anastasia <anastasyar1996@gmail.com>
2018-07-16 18:33:54 +0300

    merge master

--- src/actions/GlobalActions.js
+++ src/actions/GlobalActions.js
@@ -4,11 +4,8 @@ import GlobalReducer from '../reducers/GlobalReducer';
 
 import history from '../history';
 
-<<<<<<< HEAD
 import { SIGN_IN_PATH, INDEX_PATH, AUTH_ROUTES } from '../constants/RouterConstants';
-=======
-import { SIGN_IN_PATH, INDEX_PATH } from '../constants/RouterConstants';
->>>>>>> 2c84fc06fb1aa8621462733ebd5a9e68b7500201
+
 
 export const initAccount = (accountName) => (dispatch) => {
 	localStorage.setItem('current_account', accountName);
@@ -32,13 +29,9 @@ export const connection = () => async (dispatch) => {
 	const accountName = localStorage.getItem('current_account');
 
 	if (!accountName) {
-<<<<<<< HEAD
 		if (!AUTH_ROUTES.includes(history.location.pathname)) {
 			history.push(SIGN_IN_PATH);
 		}
-=======
-		history.push(SIGN_IN_PATH);
->>>>>>> 2c84fc06fb1aa8621462733ebd5a9e68b7500201
 		return;
 	}
 
