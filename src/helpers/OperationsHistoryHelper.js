3dd7b64ef66dfe9734c84da93f5b52e64404c53d
mikhail.shautsou <mikhail.shautsou@gmail.com>
2018-07-16 14:23:29 +0300

    add operations name

---
+++ src/helpers/OperationsHistoryHelper.js
@@ -0,0 +1,99 @@
+export const formatOperation = (operation) => {
+
+    switch (operation.name) {
+        case 'transfer':
+            break;
+        case 'limit_order_create':
+            break;
+        case 'limit_order_cancel':
+            break;
+        case 'call_order_update':
+            break;
+        case 'fill_order':
+            break;
+        case 'account_create':
+            break;
+        case 'account_update':
+            break;
+        case 'account_whitelist':
+            break;
+        case 'account_upgrade':
+            break;
+        case 'account_transfer':
+            break;
+        case 'asset_create':
+            break;
+        case 'asset_update':
+            break;
+        case 'asset_update_bitasset':
+            break;
+        case 'asset_update_feed_producers':
+            break;
+        case 'asset_issue':
+            break;
+        case 'asset_reserve':
+            break;
+        case 'asset_fund_fee_pool':
+            break;
+        case 'asset_settle':
+            break;
+        case 'asset_settle':
+            break;
+        case 'asset_global_settle':
+            break;
+        case 'asset_publish_feed':
+            break;
+        case 'witness_create':
+            break;
+        case 'witness_update':
+            break;
+        case 'proposal_create':
+            break;
+        case 'proposal_update':
+            break;
+        case 'proposal_delete':
+            break;
+        case 'withdraw_permission_create':
+            break;
+        case 'withdraw_permission_update':
+            break;
+        case 'withdraw_permission_claim':
+            break;
+        case 'withdraw_permission_delete':
+            break;
+        case 'committee_member_create':
+            break;
+        case 'committee_member_update':
+            break;
+        case 'committee_member_update_global_parameters':
+            break;
+        case 'vesting_balance_create':
+            break;
+        case 'vesting_balance_withdraw':
+            break;
+        case 'worker_create':
+            break;
+        case 'custom':
+            break;
+        case 'assert':
+            break;
+        case 'balance_claim':
+            break;
+        case 'override_transfer':
+            break;
+        case 'transfer_to_blind':
+            break;
+        case 'blind_transfer':
+            break;
+        case 'transfer_from_blind':
+            break;
+        case 'asset_settle_cancel':
+            break;
+        case 'asset_claim_fees':
+            break;
+        case 'contract':
+            break;
+        case 'contract_transfer':
+            break;
+    }
+}
