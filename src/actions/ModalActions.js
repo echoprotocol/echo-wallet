5fbf6d48ebf57045dbef348b3b162c46892bf76a
Anastasia <anastasyar1996@gmail.com>
2018-07-17 18:16:16 +0300

    rm unused

--- src/actions/ModalActions.js
+++ src/actions/ModalActions.js
@@ -1,5 +1,4 @@
 import ModalReducer from './../reducers/ModalReducer';
-import { MODAL_UNLOCK } from '../constants/ModalConstants';
 
 export const openModal = (type) => (dispatch) => {
 	dispatch(ModalReducer.actions.open({ type }));
@@ -7,8 +6,4 @@ export const openModal = (type) => (dispatch) => {
 
 export const closeModal = (type) => (dispatch) => {
 	dispatch(ModalReducer.actions.close({ type }));
-};
-
-export const openUnlockModal = () => (dispatch) => {
-	dispatch(openModal(MODAL_UNLOCK));
 };
