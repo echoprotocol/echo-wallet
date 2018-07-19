1252f377793ad6336848a485fb1f88412262ebf5
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2018-07-19 16:19:06 +0300

    EDW-73

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -22,39 +22,42 @@ class Header extends React.PureComponent {
 					<span className="icon-menu" />
 				</div>
 				<div className="page-title">{this.getTitle()}</div>
-				<div className="user-section">
-					<div className="balance">
-						<span>0.000083</span>
-						<span>ECHO</span>
+				<div className="panel-right">
+					<Button color="blue" size="small">Send</Button>
+					<div className="user-section">
+						<div className="balance">
+							<span>0.000083</span>
+							<span>ECHO</span>
+						</div>
+						<Dropdown text="sdssdsds">
+							<Dropdown.Menu>
+								<Dropdown.Item>
+									<a className="user-item">
+										<span>user 1</span>
+										<div className="balance">
+											<span>0.000083</span>
+											<span>ECHO</span>
+										</div>
+									</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a className="user-item">
+										<span>user 2</span>
+										<div className="balance">
+											<span>1083.24299901</span>
+											<span>ECHO</span>
+										</div>
+									</a>
+								</Dropdown.Item>
+								<Dropdown.Item>
+									<a className="user-panel">
+										<span className="add-account">Add account</span>
+										<span className="logout">Logout</span>
+									</a>
+								</Dropdown.Item>
+							</Dropdown.Menu>
+						</Dropdown>
 					</div>
-					<Dropdown text="sdssdsds">
-						<Dropdown.Menu>
-							<Dropdown.Item>
-								<a className="user-item">
-									<span>user 1</span>
-									<div className="balance">
-										<span>0.000083</span>
-										<span>ECHO</span>
-									</div>
-								</a>
-							</Dropdown.Item>
-							<Dropdown.Item>
-								<a className="user-item">
-									<span>user 2</span>
-									<div className="balance">
-										<span>1083.24299901</span>
-										<span>ECHO</span>
-									</div>
-								</a>
-							</Dropdown.Item>
-							<Dropdown.Item>
-								<a className="user-panel">
-									<span className="add-account">Add account</span>
-									<span className="logout">Logout</span>
-								</a>
-							</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
 				</div>
 			</div>
 		);
