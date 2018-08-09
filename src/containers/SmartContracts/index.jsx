bc7272da19df96c5c74e0aee9a0776a69b82e439
mikhail.shautsou <mikhail.shautsou@gmail.com>
2018-08-09 15:19:19 +0300

    return link

--- src/containers/SmartContracts/index.jsx
+++ src/containers/SmartContracts/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Table, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
