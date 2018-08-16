c076f6656dbdf15593b4ee1abb87fa0672a9330c
mikhail.shautsou <mikhail.shautsou@gmail.com>
2018-08-16 17:56:39 +0300

    add gextransformer

--- src/helpers/ContractHelper.js
+++ src/helpers/ContractHelper.js
@@ -1,9 +1,10 @@
 import { ChainValidation } from 'echojs-lib';
 import { keccak256 } from 'js-sha3';
-import RPL from 'rlp';
 
 import operations from '../constants/Operations';
 import { getLog, logParser } from './FormatHelper';
+
+const zero64String = '0000000000000000000000000000000000000000000000000000000000000000';
 
 export const getHash = (str) => keccak256(str);
 
@@ -13,37 +14,81 @@ export const getMethodId = (method) => {
 	return getHash(`${method.name}(${inputs})`).substr(0, 8);
 };
 
+const to64HexString = (v) => {
+	if (typeof v === 'number') {
+		return Number(v).toString(16).padStart(64, '0');
+	} else if ((ChainValidation.is_object_id(v))) {
+		return Number(v.substr(v.lastIndexOf('.') + 1)).toString(16).padStart(64, '0');
+	}
+	/*
+	['0xff', '0xff']
+0xe7984f8b
+0000000000000000000000000000000000000000000000000000000000000020
+0000000000000000000000000000000000000000000000000000000000000002
+aa00000000000000000000000000000000000000000000000000000000000000
+aa00000000000000000000000000000000000000000000000000000000000000
+['0xff']
+0xe7984f8b
+0000000000000000000000000000000000000000000000000000000000000020
+0000000000000000000000000000000000000000000000000000000000000001
+ff00000000000000000000000000000000000000000000000000000000000000
+
+	 */
+	try {
+		const arr = JSON.parse(v);
+		if (Array.isArray(arr)) {
+			return arr.reduce(() => (), '');
+		}
+		return Buffer.from(v).toString('hex').padEnd(64, '0');
+	} catch (e) {
+		return Buffer.from(v).toString('hex').padEnd(64, '0');
+	}
+
+};
+
+const encodeString = (value) => {
+	value = String(value);
+	const arg = to64HexString(value.length);
+
+	const chunks = value.match(new RegExp(`.{1,${32}}`, 'g'));
+
+	if (!chunks) return arg.concat(zero64String);
+
+	return chunks.reduce((newArg, chunk) => (newArg.concat(to64HexString(chunk))), arg);
+
+};
+
 export const getMethod = (method, args) => {
 	if (!args || !args.length) {
 		return 'Empty field';
 	}
+	let chunkCount = 0;
+	let hexStrings = '';
+	const argsString = method.inputs.reduce((hexArgs, v, i) => {
+		const arg = args[i];
+		if (!arg) return hexArgs;
+		const { type } = v;
+		chunkCount += 1;
+		if (type.search('[]') !== -1) {
+            return hexArgs.concat(to64HexString(arg));
+		} else if (type.search('int') !== -1) {
+			return hexArgs.concat(to64HexString(Number(arg)));
+		} else if (type.search('address') !== -1) {
+			return hexArgs.concat(to64HexString(arg));
+		} else if (type.search('bool') !== -1) {
+			const tmpValue = arg.toLowerCase() === 'false' ? 0 : Boolean(arg);
+			return hexArgs.concat(to64HexString(Number(tmpValue)));
+		} else if (type.search('byte') !== -1) {
 
-	const argsString = args.map((arg) => {
-		console.log('lib');
-		console.log(RPL.encode(arg));
-		console.log(RPL.encode(arg).toString('hex'));
-		let newArg = '';
-		if (!Number.isNaN(Number(arg))) {
-			newArg += Number(arg).toString(16);
-		} else if (ChainValidation.is_object_id(arg)) {
-			newArg = parseInt(arg.substr(arg.lastIndexOf('.') + 1), 10).toString(16);
-		} else if ((typeof arg) === 'string') {
-			for (let i = 0; i < arg.length; i += 1) {
-				if (!Number.isNaN(arg.charAt(i))) {
-					newArg += arg.charCodeAt(i).toString(16);
-				} else {
-					newArg += parseInt(arg.charAt(i), 10).toString(16);
-				}
-			}
+		} else if (type.search('string')) {
+			hexStrings = hexStrings.concat(encodeString(arg));
+			return hexArgs.concat(to64HexString(32 * chunkCount));
 		}
-		console.log('origin');
-		console.log(newArg);
-		return newArg.padStart(64, '0');
-	});
+		return hexArgs;
+	}, '');
 
 	method = getMethodId(method);
-	method = method.concat(argsString.join(''));
-	return method;
+	return method.concat(argsString);
 };
 
 export const checkBlockTransaction = (accountId, op, tokens) => {
