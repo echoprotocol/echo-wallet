5682ec356fdf1035cf62c18c8e88c4507d2d4d72
mikhail.shautsou <mikhail.shautsou@gmail.com>
2018-08-24 12:03:50 +0300

    fix lint

--- src/actions/SortActions.js
+++ src/actions/SortActions.js
@@ -32,7 +32,6 @@ export const initSorts = () => (dispatch, getState) => {
 		sorts[accountId] = {};
 	}
 
-	Object.entries(sorts[accountId]).map(([container, value]) => {
-		dispatch(SortReducer.actions.set({ container, value }));
-	});
+	Object.entries(sorts[accountId]).map(([container, value]) =>
+		dispatch(SortReducer.actions.set({ container, value })));
 };
