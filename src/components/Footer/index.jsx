5e3c0410a03132dc987bea1ea69769bedadd773c
Anastasia <anastasyar1996@gmail.com>
2018-09-05 14:52:25 +0300

    FIX

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,124 +1,93 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
-import { Form, Button } from 'semantic-ui-react';
-import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-class Networks extends React.Component {
+import { version } from '../../../package.json';
 
-	constructor() {
-		super();
+import { NETWORKS_PATH } from '../../constants/RouterConstants';
 
-		this.state = {
-			showCustom: false,
-			networksList: [
-				{
-					id: 1,
-					value: 'MainNet',
-				},
-				{
-					id: 2,
-					value: 'TestNet',
-				},
-				{
-					id: 3,
-					value: 'DevNet',
-				},
-				{
-					id: 'custom',
-					value: 'Custom Network',
-				},
-			],
-		};
+import { connection } from '../../actions/GlobalActions';
 
-	}
+class Footer extends React.PureComponent {
 
-	onShowCustom(id) {
-		if (id === 'custom') {
-			this.setState({ showCustom: true });
-		} else {
-			this.setState({ showCustom: false });
-		}
+
+	onReconnect() {
+		this.props.connection();
 	}
 
 	render() {
-		const { networksList } = this.state;
+		const {
+			isConnect, latency, lastBlock, instance,
+		} = this.props;
 
-		const { history } = this.props;
-
-		return (
-			<div className="sign-scroll-fix">
-				<Form className="main-form">
-					<div className="form-info">
-						<h3>Networks</h3>
-					</div>
-					<div className="field-wrap">
-						<div className="radio-list">
-							{
-								networksList.map(({ id, value }) => (
-									<div className="radio" key={id} >
-										<input
-											type="radio"
-											id={id}
-											name="network"
-											onChange={() => this.onShowCustom(id)}
-										/>
-										<label
-											className="label"
-											htmlFor={id}
-										>
-											<span className="label-text">{value}</span>
-										</label>
-									</div>
-								))
-							}
-						</div>
-						<div className={classnames('custom-network', { active: this.state.showCustom })}>
-
-							<Form.Field className={classnames('error-wrap')}>
-								<label htmlFor="address">Address</label>
-								<input placeholder="Address" disabled={!this.state.showCustom} name="address" className="ui input" />
-								<span className="error-message">asdasd</span>
-							</Form.Field>
-							<Form.Field className={classnames('error-wrap')}>
-								<label htmlFor="name">Name</label>
-								<input placeholder="Name" disabled={!this.state.showCustom} name="name" className="ui input" />
-								<span className="error-message">asdasd</span>
-							</Form.Field>
-							<Form.Field className={classnames('error-wrap')}>
-								<label htmlFor="registrator">Registrator</label>
-								<input placeholder="Registrator" disabled={!this.state.showCustom} name="registrator" className="ui input" />
-								<span className="error-message">Registrator error</span>
-							</Form.Field>
-
-						</div>
-
-
-					</div>
-					<Button
-						basic
-						type="submit"
-						color="orange"
-						className={classnames('error-wrap')}
-						content="Save"
-					/>
-					<span className="sign-nav">
-                        Return to
-						<a href="#" className="link orange pointer" onClick={history.goBack} onKeyPress={history.goBack}>Back</a>
-					</span>
-				</Form>
+		const connected = (
+			<div className="footer">
+				<ul>
+					<li>Echo.{version}</li>
+					<li className="pipeline">
+                        Latency
+						<span className="pipeline-latency"> {latency.value} ms </span>
+                        / Block
+						<span className="pipeline-block"> #{lastBlock}</span>
+					</li>
+					<li>
+						<span className="status green">
+                            Connected to
+						</span>
+						<Link className="network-link" to={NETWORKS_PATH}> {instance ? instance.url : ''}</Link>
+					</li>
+				</ul>
 			</div>
 		);
+
+		const disconnected = (
+			<div className="footer disconnected">
+				<ul>
+					<li>
+                        Check Your Connection
+						<Button type="submit" size="tiny" color="black" onClick={(e) => this.onReconnect(e)}>Try again</Button>
+					</li>
+					<li>
+						<span className="status white">Disconnected</span>
+					</li>
+				</ul>
+			</div>
+		);
+
+		return isConnect && !latency.error ? connected : disconnected;
 	}
 
 }
 
-Networks.propTypes = {
-	history: PropTypes.object.isRequired,
+Footer.propTypes = {
+	lastBlock: PropTypes.any,
+	isConnect: PropTypes.any,
+	latency: PropTypes.any,
+	instance: PropTypes.any,
+	connection: PropTypes.func.isRequired,
 };
 
-export default withRouter(connect(
-	() => ({}),
-	() => ({}),
-)(Networks));
+Footer.defaultProps = {
+	lastBlock: '',
+	isConnect: false,
+	latency: {
+		value: 0,
+		error: null,
+	},
+	instance: null,
+};
+
+
+export default connect(
+	(state) => ({
+		latency: state.echojs.getIn(['meta', 'latency']),
+		lastBlock: state.echojs.getIn(['meta', 'lastBlockNumber']),
+		isConnect: state.echojs.getIn(['system', 'isConnected']),
+		instance: state.echojs.getIn(['system', 'instance']),
+	}),
+	(dispatch) => ({
+		connection: () => dispatch(connection()),
+	}),
+)(Footer);
