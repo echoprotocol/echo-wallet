291727cbe13127f82240a695a6bb25ef3481bf69
Andrey Fedorovich <afedorovich@Andreys-iMac.local>
2018-09-06 18:44:51 +0300

    EDW-274

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -4,20 +4,28 @@ import { Menu, Sidebar } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import classnames from 'classnames';
 
 
 import { toggleBar, hideBar } from '../../actions/GlobalActions';
 import {
 	CONTRACT_LIST_PATH,
+	ADD_CONTRACT_PATH,
+	CREATE_CONTRACT_PATH,
+	CALL_CONTRACT_PATH,
 	ACTIVITY_PATH,
 	PERMISSIONS_PATH,
 	INDEX_PATH,
+	VIEW_TRANSACTION_PATH,
 } from '../../constants/RouterConstants';
 
 class SidebarMenu extends React.Component {
 
 	render() {
-
+		const {
+			location,
+		} = this.props;
+		const parsedLocation = `/${location.pathname.split('/')[1]}`;
 		return (
 			<div>
 				<Sidebar as={Menu} animation="overlay" vertical visible={this.props.visibleBar}>
@@ -55,7 +63,7 @@ class SidebarMenu extends React.Component {
 							</li>
 							<li>
 								<NavLink
-									className="sidebar-nav-link"
+									className={classnames('sidebar-nav-link', { active: [VIEW_TRANSACTION_PATH].includes(parsedLocation) })}
 									exact
 									to={ACTIVITY_PATH}
 									onClick={() => this.props.hideBar()}
@@ -67,9 +75,16 @@ class SidebarMenu extends React.Component {
 							<li>
 								<NavLink
 									exact
-									className="sidebar-nav-link"
+									className={classnames('sidebar-nav-link', {
+										active: [
+											CONTRACT_LIST_PATH,
+											ADD_CONTRACT_PATH,
+											CREATE_CONTRACT_PATH,
+											CALL_CONTRACT_PATH].includes(parsedLocation),
+									})}
 									onClick={() => this.props.hideBar()}
 									to={CONTRACT_LIST_PATH}
+
 								>
 									<span className="icon icon-contractSearch" />
 									<span className="sidebar-nav-text">Smart Contracts</span>
@@ -97,7 +112,7 @@ class SidebarMenu extends React.Component {
 }
 
 SidebarMenu.propTypes = {
-
+	location: PropTypes.object.isRequired,
 	visibleBar: PropTypes.bool.isRequired,
 	toggleBar: PropTypes.func.isRequired,
 	hideBar: PropTypes.func.isRequired,
