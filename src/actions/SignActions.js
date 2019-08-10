78c5ac0a5bb138aaa65aaac07e3f31ccfdb5b4d1
Alina <a.lozyuk@pixelplex.io>
2019-08-10 19:43:35 +0300

    [EDW-560] catch treshold error

--- src/actions/SignActions.js
+++ src/actions/SignActions.js
@@ -2,6 +2,7 @@ import { PrivateKey } from 'echojs-lib';
 import { EchoJSActions } from 'echojs-redux';
 
 import Services from '../services';
+import { toastError } from '../helpers/ToastHelper';
 
 /**
  * Get transaction signers
@@ -16,38 +17,44 @@ const getSigners = (account, keys, viewed = []) => async (dispatch) => {
 	let weight = 0;
 	let signers = [];
 
-	account.getIn(['active', 'key_auths']).forEach(([k, w]) => {
-		const key = keys
-			.find(({ accountId, publicKey }) => (publicKey === k && accountId === account.get('id')));
-		if (key && weight < account.getIn(['active', 'weight_threshold'])) {
-			weight += w;
-			signers.push(PrivateKey.fromWif(key.wif));
-		}
-	});
+	try {
+		account.getIn(['active', 'key_auths']).forEach(([k, w]) => {
+			const key = keys
+				.find(({ accountId, publicKey }) => (publicKey === k && accountId === account.get('id')));
+			if (key && weight < account.getIn(['active', 'weight_threshold'])) {
+				weight += w;
+				signers.push(PrivateKey.fromWif(key.wif));
+			}
+		});
 
-	if (weight >= account.getIn(['active', 'weight_threshold'])) {
-		return signers;
-	}
-
-	viewed.push(account.get('id'));
-	weight = await account.getIn(['active', 'account_auths']).reduce(async (wght, [id, w]) => {
-		if (viewed.includes(id)) {
-			return wght;
+		if (weight >= account.getIn(['active', 'weight_threshold'])) {
+			return signers;
 		}
 
-		try {
-			const signer = await dispatch(EchoJSActions.fetch(id));
-			const accountSigners = await dispatch(getSigners(signer, keys, viewed));
-			signers = signers.concat(accountSigners);
-			return wght + w;
-		} catch (e) {
-			//
-			return wght;
-		}
-	}, weight);
+		viewed.push(account.get('id'));
 
-	if (weight < account.getIn(['active', 'weight_threshold'])) {
-		throw new Error('Threshold is greater than the sum of keys weight available in Echo Desktop');
+		weight = await account.getIn(['active', 'account_auths']).reduce(async (wght, [id, w]) => {
+			if (viewed.includes(id)) {
+				return wght;
+			}
+
+			try {
+				const signer = await dispatch(EchoJSActions.fetch(id));
+				const accountSigners = await dispatch(getSigners(signer, keys, viewed));
+				signers = signers.concat(accountSigners);
+				return wght + w;
+			} catch (e) {
+				//
+				return wght;
+			}
+		}, weight);
+
+		if (weight < account.getIn(['active', 'weight_threshold'])) {
+			throw new Error('Threshold is greater than the sum of keys weight available in Echo Desktop');
+		}
+	} catch (error) {
+		toastError(`Transaction wasn't completed. ${error.message}`);
+		return null;
 	}
 
 	return signers;
@@ -67,5 +74,7 @@ export const signTransaction = (account, tr, password) => async (dispatch) => {
 		.all(publicKeys.map((k) => Services.getUserStorage().getWIFByPublicKey(k, { password })));
 
 	const signers = await dispatch(getSigners(signer, keys.filter((k) => k)));
+	if (!signers) return false;
 	signers.map((s) => tr.add_signer(s));
+	return true;
 };
