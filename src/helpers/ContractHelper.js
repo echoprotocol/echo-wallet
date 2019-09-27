9360a4212f858cfd8cfb3f683937e4262521fde4
Mikhail Shautsou <m.shautsou@pixelplex.io>
2019-09-27 16:44:54 +0300

    [EDW-610] Update balance subscriber (#46)

--- src/helpers/ContractHelper.js
+++ src/helpers/ContractHelper.js
@@ -2,9 +2,6 @@ import { keccak256 } from 'js-sha3';
 import BigNumber from 'bignumber.js';
 import BN from 'bn.js';
 
-import operations from '../constants/Operations';
-import { getLog, logParser } from './FormatHelper';
-
 const zero64String = '0000000000000000000000000000000000000000000000000000000000000000';
 
 export const getHash = (str) => keccak256(str);
@@ -175,32 +172,6 @@ export const getMethod = (method, args) => {
 	return code.concat(argsString, hexStrings);
 };
 
-export const checkBlockTransaction = (accountId, op, tokens) => {
-	const operation = op[0];
-	if (operation !== operations.contract_call.value) return false;
-
-	const contractId = op[1].receiver;
-	if (!contractId) return false;
-
-	const token = tokens.find((t) => (t.contractId === contractId));
-
-	if (!token) return false;
-	const registar = op[1].registrar;
-
-	return (registar && (registar === accountId));
-};
-
-export const checkTransactionResult = (accountId, result) => {
-	const log = getLog(result);
-	if (!log) return false;
-	const accountIdNumber = Number(accountId.split('.')[2]);
-	return logParser(log).some((e) => {
-		if (e.event === 'transfer') {
-			return e.params.map((p) => parseInt(p, 16)).includes(accountIdNumber);
-		}
-		return false;
-	});
-};
 
 export const getTransferCode = (id, text) => {
 	const method = keccak256('transfer(address,uint256)').substr(0, 8);
