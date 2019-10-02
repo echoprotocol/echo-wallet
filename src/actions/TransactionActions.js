853890d70d5df9bb66c0e641a393a8ee18036468
Mikhail Shautsou <m.shautsou@pixelplex.io>
2019-10-02 19:46:00 +0300

    [EDW-615, EDW-616] add frozen balances view (#55)
    * add frontend login of wieving of frozen balances
    
    * fix eslint
    
    * fix frozen-funds link in header
    
    * [EDW-615] Fix header style
    
    * add runtime freeze balance updater
    
    * update echojs-lib version
    
    * add jsdoc

--- src/actions/TransactionActions.js
+++ src/actions/TransactionActions.js
@@ -88,7 +88,7 @@ const getTransactionFee = (form, type, options) => async (dispatch, getState) =>
 			asset: feeAsset,
 		};
 	} catch (err) {
-		console.debug(err);
+		console.debug('getTransactionFee -> ', err);
 		return null;
 	}
 };
--- src/helpers/ToastHelper.js
+++ src/helpers/ToastHelper.js
@@ -24,7 +24,6 @@ export const toastInfo = (text, onUndo, onOpen) => {
 };
 
 export const toastError = (text) => {
-	console.debug(text);
 	toast.error(text, {
 		autoClose: TIME_TOAST_ANIMATION,
 		position: 'bottom-right',
