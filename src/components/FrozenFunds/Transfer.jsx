2fdd92e6c2eeacdd0a0fb4359b4b848aa37fba10
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-10-18 13:35:53 +0300

    fix

--- src/components/FrozenFunds/Transfer.jsx
+++ src/components/FrozenFunds/Transfer.jsx
@@ -102,7 +102,7 @@ class Transfer extends React.Component {
 										className="main-btn"
 										content="Freeze"
 										onClick={submit}
-										disabled={!(duration.isSelected && amount.value > 0)}
+										disabled={!(duration.isSelected)}
 									/>
 								</div>
 							</div>
