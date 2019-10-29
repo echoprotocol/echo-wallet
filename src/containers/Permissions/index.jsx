36a3805871cf95f549a2b14aa54bc3468a5347a3
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-10-29 13:36:38 +0300

    add sending on Enter

--- src/containers/Permissions/index.jsx
+++ src/containers/Permissions/index.jsx
@@ -342,7 +342,6 @@ class Permissions extends React.Component {
 		const echoRand = {
 			keys: permissionsKeys.echoRand.keys,
 		};
-		console.log(form.keys);
 		return (
 			<React.Fragment>
 				<EditModeTable
