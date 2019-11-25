4b54d06a5ab53bee835d602c0aac991e47d88356
dima <d.karpushkin@pixelplex.io>
2019-11-25 13:00:00 +0300

    fix

--- src/components/QrCode/index.jsx
+++ src/components/QrCode/index.jsx
@@ -8,21 +8,21 @@ import { BRIDGE_RECEIVE_URL } from '../../constants/GlobalConstants';
 class QrCode extends React.PureComponent {
 
 	render() {
-		const { accountName, amount, currencyId } = this.props;
+		const { receiverValue, amount, currencyId } = this.props;
 
 		return (
 			<div className="qr-section">
 				<div className="qr-wrap">
 					<QRCode
 						bgColor="#fff"
-						value={`${BRIDGE_RECEIVE_URL}${accountName}/${currencyId}/${amount}/qr-code.png`}
+						value={`${BRIDGE_RECEIVE_URL}${receiverValue}/${currencyId}/${amount}/qr-code.png`}
 						size={134}
 					/>
 				</div>
 				<div className="qr-info-wrap">
 					<div className="qr-link">
-						<a href="" target="_blank">{`${BRIDGE_RECEIVE_URL}${accountName}/${currencyId}/${amount}/widget`}</a>
-						<CopyToClipboard text={`${BRIDGE_RECEIVE_URL}${accountName}/${currencyId}/${amount}/widget`}>
+						<a href="" target="_blank">{`${BRIDGE_RECEIVE_URL}${receiverValue}/${currencyId}/${amount}/widget`}</a>
+						<CopyToClipboard text={`${BRIDGE_RECEIVE_URL}${receiverValue}/${currencyId}/${amount}/widget`}>
 							<button className="link-copy-btn icon-icopy-tiny" />
 						</CopyToClipboard>
 					</div>
@@ -38,7 +38,7 @@ class QrCode extends React.PureComponent {
 }
 
 QrCode.propTypes = {
-	accountName: PropTypes.string.isRequired,
+	receiverValue: PropTypes.string.isRequired,
 	currencyId: PropTypes.string,
 	amount: PropTypes.string,
 };
