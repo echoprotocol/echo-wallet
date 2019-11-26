abe9550be9a793742629c0f3e3cb3c61706a3a80
Artsiom Dziaryba <a.dziaryba@pixelplex.io>
2019-11-26 14:04:23 +0300

    prikol

--- src/components/Fields/AddressField.jsx
+++ src/components/Fields/AddressField.jsx
@@ -8,7 +8,7 @@ class AddressField extends React.Component {
 
 	render() {
 		const {
-			amount, fee, assetDropdown, labelText,
+			address, labelText,
 		} = this.props;
 
 		return (
@@ -32,20 +32,13 @@ class AddressField extends React.Component {
 						<input
 							className="address"
 							placeholder="Address"
-							value={amount.value}
+							value={address ? address.address : ''}
 							name="address"
 							onChange={() => {}}
 							onFocus={() => {}}
 							onBlur={() => {}}
 						/>
-						{
-							this.renderErrorStaus(assetDropdown, amount.error, fee.error)
-						}
 					</div>
-					{
-						amount.error || fee.error ?
-							<span className="error-message">{amount.error || fee.error}</span> : null
-					}
 				</Input>
 
 			</Form.Field>
@@ -55,34 +48,14 @@ class AddressField extends React.Component {
 }
 
 AddressField.propTypes = {
-	fees: PropTypes.array.isRequired,
-	form: PropTypes.string.isRequired,
-	fee: PropTypes.object,
-	assets: PropTypes.object,
-	tokens: PropTypes.object.isRequired,
-	amount: PropTypes.object.isRequired,
-	currency: PropTypes.object,
-	isAvailableBalance: PropTypes.bool.isRequired,
-	amountInput: PropTypes.func.isRequired,
-	setFormError: PropTypes.func.isRequired,
-	setFormValue: PropTypes.func.isRequired,
-	setValue: PropTypes.func.isRequired,
-	setDefaultAsset: PropTypes.func.isRequired,
-	setContractFees: PropTypes.func.isRequired,
-	getTransferFee: PropTypes.func.isRequired,
-	assetDropdown: PropTypes.bool,
 	labelText: PropTypes.string,
-	receive: PropTypes.bool,
+	address: PropTypes.object,
 };
 
 
 AddressField.defaultProps = {
-	currency: null,
-	fee: {},
-	assets: null,
-	assetDropdown: true,
-	labelText: 'Amount',
-	receive: false,
+	labelText: 'Address',
+	address: null,
 };
 
 export default AddressField;
