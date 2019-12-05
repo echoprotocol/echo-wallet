5e28283ae35e92b9b03b15848629a7ed369bc200
kosAndrey22 <a.kosobuckiy@pixelplex.io>
2019-12-05 15:44:34 +0300

    [EDW-836] Add opportunity to register with custom wif and/or public key (#202)
    add opportunity to register with custom wif and/or public key

--- src/containers/SignUp/index.jsx
+++ src/containers/SignUp/index.jsx
@@ -17,7 +17,7 @@ import { SIGN_IN_PATH } from '../../constants/RouterConstants';
 import { FORM_SIGN_UP } from '../../constants/FormConstants';
 
 import { generateWIF, createAccount } from '../../actions/AuthActions';
-import { setFormValue, setValue, clearForm } from '../../actions/FormActions';
+import { setFormValue, setValue, clearForm, setFormError } from '../../actions/FormActions';
 
 class SignUp extends React.Component {
 
@@ -31,15 +31,15 @@ class SignUp extends React.Component {
 
 	onCreate(password) {
 		const {
-			accountName, generatedWIF, confirmWIF, isAddAccount,
+			accountName, generatedWIF, confirmWIF, isAddAccount, isCustomWIF, userWIF,
 		} = this.props;
-
 		this.props.createAccount({
 			accountName: accountName.value.trim(),
-			generatedWIF: generatedWIF.value.trim(),
-			confirmWIF: confirmWIF.value.trim(),
+			generatedWIF: isCustomWIF ? userWIF.value.trim() : generatedWIF.value.trim(),
+			confirmWIF: isCustomWIF ? userWIF.value.trim() : confirmWIF.value.trim(),
 			password,
-		}, isAddAccount);
+		}, isAddAccount, isCustomWIF);
+
 	}
 
 	isDisabledSubmit() {
@@ -48,8 +48,19 @@ class SignUp extends React.Component {
 			accountName,
 			generatedWIF,
 			confirmWIF,
+			userPublicKey,
+			userWIF,
+			isCustomWIF,
 		} = this.props;
 
+		if (isCustomWIF) {
+			if ((!accountName.value || accountName.error) ||
+				userPublicKey.error || userWIF.error ||
+				!(userPublicKey.value || userWIF.value) || !accepted) {
+				return true;
+			}
+			return false;
+		}
 		if ((!accountName.value || accountName.error) ||
 			(!generatedWIF.value || generatedWIF.error) ||
 			(!confirmWIF.value || confirmWIF.error) || !accepted) {
@@ -60,7 +71,7 @@ class SignUp extends React.Component {
 	}
 
 	render() {
-		const {	location, loading } = this.props;
+		const { location, loading } = this.props;
 
 		const { isAddAccount } = qs.parse(location.search);
 
@@ -93,8 +104,13 @@ class SignUp extends React.Component {
 									accountName={this.props.accountName}
 									generatedWIF={this.props.generatedWIF}
 									confirmWIF={this.props.confirmWIF}
+									setValue={this.props.setValue}
 									setFormValue={this.props.setFormValue}
+									setFormError={this.props.setFormError}
 									clearForm={this.props.clearForm}
+									isCustomWIF={this.props.isCustomWIF}
+									userPublicKey={this.props.userPublicKey}
+									userWIF={this.props.userWIF}
 								/>
 
 								<AdditionalOptions loading={loading} />
@@ -105,7 +121,7 @@ class SignUp extends React.Component {
 								/>
 								<div className="form-panel">
 									<span className="sign-nav">
-									Have an account?
+										Have an account?
 										<Link
 											className={classnames('link', 'orange', { disabled: loading })}
 											to={`${SIGN_IN_PATH}${isAddAccount ? '?isAddAccount=true' : ''}`}
@@ -131,6 +147,7 @@ class SignUp extends React.Component {
 
 SignUp.propTypes = {
 	loading: PropTypes.bool,
+	isCustomWIF: PropTypes.bool,
 	accepted: PropTypes.bool,
 	isAddAccount: PropTypes.any,
 	history: PropTypes.object.isRequired,
@@ -138,9 +155,12 @@ SignUp.propTypes = {
 	accountName: PropTypes.object.isRequired,
 	generatedWIF: PropTypes.object.isRequired,
 	confirmWIF: PropTypes.object.isRequired,
+	userPublicKey: PropTypes.object.isRequired,
+	userWIF: PropTypes.object.isRequired,
 	generateWIF: PropTypes.func.isRequired,
 	createAccount: PropTypes.func.isRequired,
 	setFormValue: PropTypes.func.isRequired,
+	setFormError: PropTypes.func.isRequired,
 	setValue: PropTypes.func.isRequired,
 	clearForm: PropTypes.func.isRequired,
 };
@@ -149,20 +169,25 @@ SignUp.defaultProps = {
 	loading: false,
 	accepted: false,
 	isAddAccount: false,
+	isCustomWIF: false,
 };
 
 export default connect(
 	(state) => ({
 		loading: state.form.getIn([FORM_SIGN_UP, 'loading']),
+		isCustomWIF: state.form.getIn([FORM_SIGN_UP, 'isCustomWIF']),
 		accepted: state.form.getIn([FORM_SIGN_UP, 'accepted']),
 		accountName: state.form.getIn([FORM_SIGN_UP, 'accountName']),
 		generatedWIF: state.form.getIn([FORM_SIGN_UP, 'generatedWIF']),
 		confirmWIF: state.form.getIn([FORM_SIGN_UP, 'confirmWIF']),
+		userPublicKey: state.form.getIn([FORM_SIGN_UP, 'userPublicKey']),
+		userWIF: state.form.getIn([FORM_SIGN_UP, 'userWIF']),
 	}),
 	(dispatch) => ({
 		generateWIF: () => dispatch(generateWIF()),
-		createAccount: (value, isAdd) => dispatch(createAccount(value, isAdd)),
+		createAccount: (value, isAdd, isCustom) => dispatch(createAccount(value, isAdd, isCustom)),
 		setFormValue: (field, value) => dispatch(setFormValue(FORM_SIGN_UP, field, value)),
+		setFormError: (field, value) => dispatch(setFormError(FORM_SIGN_UP, field, value)),
 		setValue: (field, value) => dispatch(setValue(FORM_SIGN_UP, field, value)),
 		clearForm: () => dispatch(clearForm(FORM_SIGN_UP)),
 	}),
