21cd0d59018b6b4ae41e1fc47734675efbdd3fb9
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-12-09 18:54:41 +0300

    add timer

--- src/components/Modals/ModalEditPermissions.jsx
+++ src/components/Modals/ModalEditPermissions.jsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/no-did-update-set-state */
 import React from 'react';
 import { Modal, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import Countdown from 'react-countdown-now';
 import FocusLock from 'react-focus-lock';
 
 import PasswordInput from '../PasswordInput';
@@ -12,26 +12,30 @@ class ModalEditPermissions extends React.Component {
 		super(props);
 		this.state = {
 			agree: false,
-			timerIsOn: false,
 			timerComplete: false,
-			show: false,
-			changeVisiblity: false,
-		};
-	}
-	static getDerivedStateFromProps(nextProps, prevState) {
-		return nextProps.show !== prevState.show ? {
-			show: nextProps.show,
-			changeVisiblity: true,
-			timerComplete: false,
-			agree: false,
-		} : {
-			changeVisiblity: false,
+			timer: null,
+			time: props.warningTime,
 		};
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		return (nextState.changeVisiblity) ? true : !(nextState.timerIsOn && !nextState.timerComplete);
+
+	componentDidUpdate(prevProps) {
+		if (this.props.show && !prevProps.show) {
+			const t = setInterval(() => {
+				this.setState({ time: this.state.time - 1 }, () => {
+					if (this.state.time === 0) {
+						clearTimeout(this.state.timer);
+						this.setState({ timerComplete: true });
+					}
+				});
+			}, 1000);
+			this.setState({ timer: t });
+		} else if (!this.props.show && prevProps.show) {
+			clearTimeout(this.state.timer);
+			this.setState({ timer: null, time: this.props.warningTime, timerComplete: false });
+		}
 	}
+
 
 	onCheck(e) {
 		this.setState({ agree: e.currentTarget.checked });
@@ -55,7 +59,7 @@ class ModalEditPermissions extends React.Component {
 		this.props.change(e.target.value.trim());
 	}
 
-	countdown({ seconds }) {
+	countdown(seconds) {
 		return !this.state.timerComplete &&
 			<div className="countdown">
 				<div className="countdown-text">{seconds}</div>
@@ -64,7 +68,7 @@ class ModalEditPermissions extends React.Component {
 
 	render() {
 		const {
-			show, error, disabled, warningTime, password,
+			show, error, disabled, password,
 		} = this.props;
 
 		const { agree, timerComplete } = this.state;
@@ -81,10 +85,10 @@ class ModalEditPermissions extends React.Component {
 					</div>
 					<form className="modal-body">
 						<div className="info-text">
-								Please, keep in mind that uncontrolled changes may lead to
-								loosing access to the wallet or restricting your actions within it.
-								Be careful with editing permissions and adding the accounts to manage the wallet,
-								ensuring that you grant permissions only to the accounts you trust.
+							Please, keep in mind that uncontrolled changes may lead to
+							loosing access to the wallet or restricting your actions within it.
+							Be careful with editing permissions and adding the accounts to manage the wallet,
+							ensuring that you grant permissions only to the accounts you trust.
 						</div>
 						<div className="check-list">
 							<div className="check">
@@ -110,7 +114,7 @@ class ModalEditPermissions extends React.Component {
 								onKeyPress={(e) => this.onForgot(e)}
 								tabIndex="0"
 							>
-							Forgot password?
+								Forgot password?
 							</a>
 							<Button
 								type="submit"
@@ -118,15 +122,7 @@ class ModalEditPermissions extends React.Component {
 								onClick={(e) => this.onSuccess(e)}
 								disabled={(disabled) || !(agree && timerComplete)}
 							>
-								<Countdown
-									date={Date.now() + (warningTime * 1000)}
-									renderer={(props) => this.countdown(props)}
-									onStart={() => this.setState({ timerIsOn: true })}
-									onComplete={() => this.setState({
-										timerComplete: true,
-										timerIsOn: false,
-									})}
-								/>
+								{this.countdown(this.state.time)}
 								{(agree && timerComplete) ? 'Go to edit mode' : 'READ PLEASE'}
 							</Button>
 						</div>
@@ -142,6 +138,7 @@ ModalEditPermissions.propTypes = {
 	show: PropTypes.bool,
 	disabled: PropTypes.bool,
 	error: PropTypes.string,
+	password: PropTypes.string.isRequired,
 	change: PropTypes.func.isRequired,
 	unlock: PropTypes.func.isRequired,
 	forgot: PropTypes.func.isRequired,
