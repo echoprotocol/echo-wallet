83623825a37ec89254e6afb1257ac3e04c8e654b
aFedorovich <a.fedorovich@pixelplex.io>
2019-12-16 17:31:52 +0300

    [EDW-879] add accept modal

--- src/components/Modals/ModalAcceptIncomingConnections.jsx
+++ src/components/Modals/ModalAcceptIncomingConnections.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import { Modal } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
@@ -7,6 +6,7 @@ import FocusLock from 'react-focus-lock';
 
 import { closeModal } from '../../actions/ModalActions';
 import { MODAL_ACCEPT_INCOMING_CONNECTIONS } from '../../constants/ModalConstants';
+import icAccept from '../../assets/images/ic-accept.svg';
 
 class ModalInfoWallet extends React.Component {
 
@@ -18,19 +18,56 @@ class ModalInfoWallet extends React.Component {
 	render() {
 		const { show } = this.props;
 		return (
-			<Modal className="accept-incoming-connections" open>
+			<Modal
+				className="modal-wrap"
+				open={show}
+			>
 				<FocusLock autoFocus={false}>
-					<div className="modal-content">
-						<button
-							className="icon-close"
-							onClick={(e) => this.onClose(e)}
-						/>
-						<div className="modal-header">
-							Do you want the application “Echo Wallet” to accept incoming network connections?
-						</div>
-						<div className="modal-body">
+					<button
+						className="icon-close"
+						onClick={(e) => this.onClose(e)}
+					/>
+					<div className="modal-header">
+						<h2 className="modal-header-title">
+							Do you want the application “Echo Wallet”<br />
+							to accept incoming network connections?
+						</h2>
+					</div>
+					<div className="accept-connections modal-body">
+						<h3 className="accept-connections-title">
+							Accept incoming network connections and <br />
+							<span className="bold">enjoy all the benefits of running local node *</span>
+						</h3>
+						<ul className="accept-connections-list">
+							<li>
+								<img src={icAccept} alt="" />
+								<span className="list-item-content">Become consensus participant</span>
+							</li>
+							<li>
+								<img src={icAccept} alt="" />
+								<span className="list-item-content">Earn block rewards</span>
+							</li>
+							<li>
+								<img src={icAccept} alt="" />
+								<span className="list-item-content">Generate new ECHO accounts</span>
+							</li>
+							<li>
+								<img src={icAccept} alt="" />
+								<span className="list-item-content">
+									Run on your PC locally and<br /> safeguard your connection
+								</span>
+							</li>
+						</ul>
 
-							dd
+						<div className="accept-connections-hint">
+							* This setting can be changed in the Firewall pane of Security
+							& Privacy preferences anytime.
+						</div>
+						<div className="form-panel">
+							<button className="transparet-btn grey">Dismiss</button>
+							<button autoFocus className="blue-btn">
+								CONFIRM
+							</button>
 						</div>
 					</div>
 				</FocusLock>
