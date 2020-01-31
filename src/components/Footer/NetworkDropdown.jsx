a3912c464ef604b941d6cb52345188998d788b1f
kosAndrey22 <a.kosobuckiy@pixelplex.io>
2020-01-31 13:13:04 +0300

    [EDW-986] Fix dropdown network title (#319)

--- src/components/Footer/NetworkDropdown.jsx
+++ src/components/Footer/NetworkDropdown.jsx
@@ -234,8 +234,8 @@ class Network extends React.PureComponent {
 					<div className="trigger">
 						<span className="description">
 							{ disconnected ?
-								<FormattedMessage id="footer.network_section.title" /> :
-								<FormattedMessage id="footer.network_section.disconnected" />
+								<FormattedMessage id="footer.network_section.disconnected" /> :
+								<FormattedMessage id="footer.network_section.title" />
 							}
 						</span>
 						{this.renderNode()}
